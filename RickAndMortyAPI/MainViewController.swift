b0f414462b088bd437d6da8d5482f05adbd39665
Ruslan Shigapov <ilgamovich@gmail.com>
2024-02-07 11:32:17 +0300

    Added customized project

---
+++ RickAndMortyAPI/MainViewController.swift
@@ -0,0 +1,16 @@
+//
+//  MainViewController.swift
+//  RickAndMortyAPI
+//
+//  Created by Ruslan Shigapov on 07.02.2024.
+//
+
+import UIKit
+
+final class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .cyan
+    }
+}
