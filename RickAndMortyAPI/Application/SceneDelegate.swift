06b01df9763e491ebce6e3b876f31d93b941f2c8
Ruslan Shigapov <ilgamovich@gmail.com>
2024-02-07 11:39:00 +0300

    Deleted storyboards

--- RickAndMortyAPI/Application/SceneDelegate.swift
+++ RickAndMortyAPI/Application/SceneDelegate.swift
@@ -16,7 +16,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         willConnectTo session: UISceneSession,
         options connectionOptions: UIScene.ConnectionOptions
     ) {
-        guard let _ = (scene as? UIWindowScene) else { return }
+        guard let windowScene = scene as? UIWindowScene else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.makeKeyAndVisible()
+        window?.rootViewController = UINavigationController(
+            rootViewController: MainViewController())
     }
 }
 
