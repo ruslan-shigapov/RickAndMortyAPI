7db9eaade40ee93fc7a20d7b74cf5bc0af51bfed
Ruslan Shigapov <ilgamovich@gmail.com>
2024-02-07 18:08:57 +0300

    Created DetailsVC and view model

---
+++ RickAndMortyAPI/DetailsScreen/DetailsViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailsViewController.swift
+//  RickAndMortyAPI
+//
+//  Created by Ruslan Shigapov on 07.02.2024.
+//
+
+import UIKit
+
+final class DetailsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ RickAndMortyAPI/DetailsScreen/DetailsViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  DetailsViewModel.swift
+//  RickAndMortyAPI
+//
+//  Created by Ruslan Shigapov on 07.02.2024.
+//
+
+import Foundation
+
+final class DetailsViewModel {
+    
+}
